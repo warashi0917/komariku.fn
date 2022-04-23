023625d7091d467195651425c8267c877662bcec
warashi0917 <96465316+warashi0917@users.noreply.github.com>
2022-04-23 09:45:05 +0900

    Add files via upload
    hero image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-'use strict'
+'use strict';
 
 {
+  function play() {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      if (currentIndex > images.length - 1) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
+      play();
+    }, 5000);
+
+  }
+
+  const images = document.querySelectorAll('.hero img');
+  let currentIndex = 0;
   
+  play();
 }
